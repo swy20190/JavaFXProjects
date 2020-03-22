f388ab3d8047d2acf0b179d36247971646a03242
swy20190 <K201410109@163.com>
2020-03-22 21:56:03 +0800

    14.7 finished

--- 14.6/src/sample/Main.java
+++ 14.6/src/sample/Main.java
@@ -4,27 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Pane pane = new Pane();
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                Rectangle tmp = new Rectangle();
-                tmp.xProperty().bind(pane.widthProperty().divide(8).multiply(j));
-                tmp.yProperty().bind(pane.heightProperty().divide(8).multiply(i));
-                tmp.widthProperty().bind(pane.widthProperty().divide(8));
-                tmp.heightProperty().bind(pane.heightProperty().divide(8));
-                if((i%2==0&&j%2==0)||(i%2==1&&j%2==1))
-                    tmp.setFill(Color.WHITE);
-                else
-                    tmp.setFill(Color.BLACK);
+        for(int i=0;i<10;i++){
+            for(int j=0;j<10;j++){
+                //rectangle tmp = new Rectangle();\
+                TextField tmp = new TextField();
+                //tmp.set().bind(pane.widthProperty().divide(8).multiply(j));
+                //tmp.yProperty().bind(pane.heightProperty().divide(8).multiply(i));
+
+                tmp.setLayoutX(40*j+5);
+                tmp.setLayoutY(25*i+5);
+                tmp.setMaxWidth(40);
+                tmp.setMaxHeight(10);
+                tmp.setText((int)(Math.random()*2)+"");
+                tmp.setCenterShape(true);
                 pane.getChildren().add(tmp);
             }
         }

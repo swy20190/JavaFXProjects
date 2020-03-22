8ab74c1caa27ce4832a73c5dee19d78477655f81
swy20190 <K201410109@163.com>
2020-03-22 20:07:01 +0800

    14.4finished

--- 14.4/src/sample/Main.java
+++ 14.4/src/sample/Main.java
@@ -2,17 +2,39 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+
+import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FlowPane pane = new FlowPane();
+        pane.setOrientation(Orientation.HORIZONTAL);
+        for(int i=0;i<5;i++){
+            Text tmp = new Text("Java");
+            tmp.setFont(Font.font("Times New Roman", FontWeight.BLACK, FontPosture.ITALIC,22));
+            Color jj = new Color(Math.random(),Math.random(),Math.random(),Math.random());
+            tmp.setFill(jj);
+            tmp.setRotate(90);
+            pane.getChildren().add(tmp);
+        }
+        //pane.setRotate(90);
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("problem 14.4");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
